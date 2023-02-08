597bffe2e605dbb87dd604bdefeef1ef045ef48e
Kishore-2572 <kishoren2572@gmail.com>
2023-02-08 09:14:26 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const dotenv=require('dotenv')
+dotenv.config()
+const app = express()
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT, () => console.log(`Funworld listening on port ${process.env.PORT}!`))
